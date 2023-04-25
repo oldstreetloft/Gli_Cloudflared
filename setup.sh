dba451523af64721be4e08b5bfd2438c3a1150c7
laptop <laptop@laptops-MacBook-Pro.local>
2023-04-25 13:53:05 -0400

    Working

--- setup.sh
+++ setup.sh
@@ -1,11 +1,51 @@
 #!/bin/bash
+
 prompt_user() {
     read -p "Enter the IP address: " ip_address
-    read -p "Enter the password: " password
     read -p "Enter CFD Token: " token
 }
 
 prompt_user
-echo $ip_address
-echo $password
-echo $token
+ssh root@$ip_address << ENDSSH
+curl -O -L https://github.com/cloudflare/cloudflared/releases/download/2023.4.2/cloudflared-linux-arm
+chmod +x cloudflared-linux-arm
+mv cloudflared-linux-arm /usr/bin/cloudflared
+
+cat > /etc/init.d/cloudflared << EOF
+#!/bin/sh /etc/rc.common
+
+USE_PROCD=1
+START=95
+STOP=01
+
+cfd_init="/etc/init.d/cloudflared"
+cfd_token="$token"
+
+boot()
+{
+    ubus -t 30 wait_for network.interface network.loopback 2>/dev/null
+    rc_procd start_service
+}
+
+start_service() {
+if [ \\\$("\\\${cfd_init}" enabled; printf "%u" ${?}) -eq 0 ]
+    then
+        procd_open_instance
+        procd_set_param command /usr/bin/cloudflared --no-autoupdate tunnel run --token \\\${cfd_token}
+        procd_set_param stdout 1
+        procd_set_param stderr 1
+        procd_set_param respawn \\\${respawn_threshold:-3600} \\\${respawn_timeout:-5} \\\${respawn_retry:-5}
+        procd_close_instance
+    fi
+}
+
+stop_service() {
+    pidof cloudflared && kill -SIGINT \\\`pidof cloudflared\\\`
+}
+EOF
+
+chmod +x /etc/init.d/cloudflared
+/etc/init.d/cloudflared enable
+/etc/init.d/cloudflared start
+echo 'Set split tunnel in Cloudflare zero trust portal under settings -> warp app'
+ENDSSH
