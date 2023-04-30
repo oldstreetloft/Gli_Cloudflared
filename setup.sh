c61d664516d18b7a43baf5cca4d5e1d3ec7b9b7c
oldstreetloft <consulting@oldstreetloft.io>
2023-04-30 11:32:30 -0400

    added \n newline

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ test_conn() {
     if [[ $latest ]]; then
         printf "You are connected to the internet.\n"
         printf "Latest cloudflared version: $latest\n"
-        printf "Latest GH download URL: $down_url\n\n"
+        printf "Latest GH download URL: \n$down_url\n\n"
     else
         printf "ERROR:\nYou are not connected to the internet.\n"
         printf "Please ensure internet connectivity and try again.\n\n"
